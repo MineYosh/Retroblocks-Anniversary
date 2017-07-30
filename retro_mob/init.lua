ab7e08a6ab539137103bbe6a8f99801d09829fdd
Miguel Isaac <mineyoshist@protonmail.ch>
2017-07-29 19:59:27 -0500

    Update init.lua

--- retro_mob/init.lua
+++ retro_mob/init.lua
@@ -4,6 +4,16 @@ local mobpath = minetest.get_modpath("retro_mob")
 
 if minetest.get_modpath("mobs") then
 	print(mob_incompatibility)
+elseif minetest.get_modpath("mila") then
+	dofile(mobpath.."/settings.lua")
+	dofile(mobpath.. "/yosho.lua") 
+	dofile(mobpath.. "/spadedino.lua") 
+	dofile(mobpath.. "/toaddo.lua") 
+	dofile(mobpath.. "/bonesdry.lua") 
+	dofile(mobpath.. "/blonky.lua") 
+	dofile(mobpath.. "/clide.lua") 
+	dofile(mobpath.. "/Onky.lua") 
+	dofile(mobpath.. "/Pinkyo.lua") 
 else
 	dofile(mobpath.."/api.lua")
 	dofile(mobpath.."/settings.lua")
@@ -15,5 +25,4 @@ else
 	dofile(mobpath.. "/clide.lua") 
 	dofile(mobpath.. "/Onky.lua") 
 	dofile(mobpath.. "/Pinkyo.lua") 
-
 end
